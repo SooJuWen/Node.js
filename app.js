a59ae8d017dd816e75dbcdd2e19b5263e58d547f
SooJuWen <soojw-wp19@student.tarc.edu.my>
2022-11-03 21:24:37 +0800

    EJS second commit

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ app.get('/', (req, res) => {
     //res.send('<p>Hello Express<p>');
     //res.sendFile('./views/index.html', {root: __dirname});
     //ejs
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'Home', blogs});
 })
 
 
@@ -21,11 +26,12 @@ app.get('/about', (req, res) => {
     //res.send('<p>Hello Express<p>');
     //res.sendFile('./views/about.html', {root: __dirname});
     //ejs
-    res.render('about');
+    
+    res.render('about', {title: 'About'});
 })
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create a new blog'});
 })
 
 //redirects
@@ -40,5 +46,5 @@ app.get('/about-us', (req, res) => {
 app.use((req, res) => {
     //res.status(404).sendFile('./views/404.html', {root: __dirname});
     //ejs
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404'});
 })
