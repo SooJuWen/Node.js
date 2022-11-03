a1ebb59da2519ccea3c023da80ac4bc8cffa52fe
SooJuWen <soojw-wp19@student.tarc.edu.my>
2022-11-03 18:23:21 +0800

    EJS first commit

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p>Hello Express<p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    //res.sendFile('./views/index.html', {root: __dirname});
+    //ejs
+    res.render('index');
 })
 
 
 app.get('/about', (req, res) => {
     //res.send('<p>Hello Express<p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    //res.sendFile('./views/about.html', {root: __dirname});
+    //ejs
+    res.render('about');
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 })
 
 //redirects
@@ -27,5 +38,7 @@ app.get('/about-us', (req, res) => {
 // Express run code from top to bottom, so if all get requests above
 // didn't match then it will fire this function to send the html page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    //res.status(404).sendFile('./views/404.html', {root: __dirname});
+    //ejs
+    res.status(404).render('404');
 })
