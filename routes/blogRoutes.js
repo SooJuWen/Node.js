3216151bed3b26417915fae15f080d22af78dd56
SooJuWen <soojw-wp19@student.tarc.edu.my>
2022-11-06 14:52:21 +0800

    MVC commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,55 +1,16 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController')
 const router = express.Router();
 
-router.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1})
-    .then((result) => {
-        res.render('index', {title: 'All Blogs', blogs: result})
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
+router.get('/blogs', blogController.blog_index)
 
-router.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
+router.post('/blogs', blogController.blog_create_post)
 
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-router.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new blog'});
-})
+router.get('/blogs/create', blogController.blog_create_get)
 
 // use :xxx for variable id to handle the changeable part on the address
-router.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog Details'})
-        })
-        .catch(err => {
-            console.log(err);
-        });
-})
+router.get('/blogs/:id', blogController.blog_details)
 
-router.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' })
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
+router.delete('/blogs/:id', blogController.blog_delete)
 
 module.exports = router;
