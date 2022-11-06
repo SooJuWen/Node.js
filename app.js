80cb96b9ba89ce07f33a98a3c9dfc609af783043
SooJuWen <soojw-wp19@student.tarc.edu.my>
2022-11-06 14:29:32 +0800

    express router commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { render } = require('ejs');
 const express = require('express');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
@@ -73,51 +73,7 @@ app.get('/', (req, res) => {
 })
 
 //blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1})
-    .then((result) => {
-        res.render('index', {title: 'All Blogs', blogs: result})
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-// use :xxx for variable id to handle the changeable part on the address
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog Details'})
-        })
-        .catch(err => {
-            console.log(err);
-        });
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' })
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
+app.use(blogRoutes);
 
 app.get('/about', (req, res) => {
     //res.send('<p>Hello Express<p>');
@@ -126,9 +82,6 @@ app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
 })
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new blog'});
-})
 
 //redirects
 app.get('/about-us', (req, res) => {
